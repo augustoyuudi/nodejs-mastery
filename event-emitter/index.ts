d6842eb7b2fbcdae5305261dc196068f9ca71f29
augustoyuudi <augustoyuudi@gmail.com>
2024-02-15 20:47:16 -0300

    feat: pass params and this to event

--- event-emitter/index.ts
+++ event-emitter/index.ts
@@ -2,8 +2,11 @@ import { EventEmitter } from 'node:events';
 
 const eventEmitter = new EventEmitter();
 
-eventEmitter.on('my-event', (a, b) => {
+eventEmitter.on('my-event', function(this: typeof EventEmitter, a, b) {
+  console.log(a);
+  console.log(b);
+  console.log(this);
   console.log('my event occurred!');
 });
 
-eventEmitter.emit('my-event');
+eventEmitter.emit('my-event', 10, 'hello');
