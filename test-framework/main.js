a56fbe6a03f61ae1a813b6df78da2e6860ea7414
augustoyuudi <augustoyuudi@gmail.com>
2024-03-11 21:40:53 -0300

    feat: init test-framework proj

---
+++ test-framework/main.js
@@ -0,0 +1,32 @@
+import HasteMap from 'jest-haste-map';
+import { cpus } from 'os';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const root = dirname(fileURLToPath(import.meta.url));
+
+const hasteMapOptions = {
+  extensions: ['.js'],
+  maxWorkers: cpus().length,
+  name: 'test-framework',
+  platforms: [],
+  rootDir: root,
+  roots: [root],
+};
+
+const hasteMap = new HasteMap.default(hasteMapOptions);
+
+await hasteMap.setupCachePath(hasteMapOptions);
+
+const { hasteFS } = await hasteMap.build();
+const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
+
+// console.log(testFiles.entries());
+
+await Promise.all(
+  Array.from(testFiles).map(async (testFile) => {
+    const code = fs.readFileSync(testFile, 'utf-8');
+    console.log(testFile + '\n' + code);
+  })
+);
