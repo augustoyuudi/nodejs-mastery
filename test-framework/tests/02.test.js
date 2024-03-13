efcaabe15e1f84bcbc2aa27257003d5f85a47718
augustoyuudi <augustoyuudi@gmail.com>
2024-03-13 20:50:06 -0300

    feat: actually run test file

--- test-framework/tests/02.test.js
+++ test-framework/tests/02.test.js
@@ -1,1 +1,4 @@
-console.log('hey');
+expect(1+1).toBe(2);
+expect(1+1).toBe(3);
+expect(1+1).toBe(4);
+expect(1+1).toBe(5);
--- test-framework/worker.js
+++ test-framework/worker.js
@@ -1,7 +1,30 @@
 const fs = require('fs');
 
+const expect = (received) => {
+  return {
+    toBe(expected) {
+      if (received !== expected) {
+        throw new Error(`Expected ${expected} but received ${received}`);
+      }
+      return true;
+    }
+  };
+};
+
 exports.runTest = async function (testFile) {
   const code = await fs.promises.readFile(testFile, 'utf-8');
 
-  return `worker id: ${process.env.JEST_WORKER_ID}\nfile: ${testFile}:\n${code}`;
+  const testResult = {
+    success: false,
+    errorMessage: null,
+  };
+
+  try {
+    eval(code); // eval has access to the scope around
+    testResult.success = true;
+  } catch (error) {
+    testResult.errorMessage = error.message;
+  }
+
+  return testResult;
 }
